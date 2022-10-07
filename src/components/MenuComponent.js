6fa307152e190f204790abf288a053d6db0d22c3
Margaret Baber <mbbaber@Maggies-MacBook-Pro.local>
2022-10-07 18:10:53 +0200

    display dish details on click

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,15 +6,39 @@ class Menu extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            selectedDish: null
+        }
     };
+
+    onDishSelect(dish) {
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        } else {
+            return(
+                <div></div>
+            )
+        }
+    }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
                 <Card>
-                    <Card>
+                    <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
                         <CardImgOverlay></CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
@@ -29,6 +53,9 @@ class Menu extends Component {
             <div className='container'>
                 <div className='row'>
                     {menu}
+                </div>
+                <div className='row'>
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
