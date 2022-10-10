1bf540b169ef64a9fb729ae9194f02136220427e
Margaret Baber <mbbaber@Maggies-MacBook-Pro.local>
2022-10-10 11:26:09 +0200

    put features on homepage

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,11 @@
+import Contact from './ContactComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import React, {Component} from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -14,6 +18,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       selectedDishId: null
     }
   }
@@ -25,16 +32,21 @@ class Main extends Component {
   render() {
     const HomePage = () => {
         return(
-            <Home />
+            <Home 
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
         )
     }
     return (
     <div>
         <Header/>
             <Routes>
-                <Route path="/home" element={<Home/>} />
+                <Route path="/home" element={<HomePage/>} />
                 <Route exact path="/menu" element={<Menu dishes={this.state.dishes}/>}/>
                 <Route path="/" element={<Navigate to="/home"/>}/>
+                <Route exact path='/contactus' element={<Contact />} />
             </Routes>
         {/* <Menu dishes={this.state.dishes}
             onClick={(dish) => this.onDishSelect(dish)}
