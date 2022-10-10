feaef80a226e4ac1c608c529cf3617682a00c0f9
Margaret Baber <mbbaber@Maggies-MacBook-Pro.local>
2022-10-10 10:56:03 +0200

    add react router and links

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
 import React, {Component} from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import {Routes, Route, Navigate} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -16,18 +18,28 @@ class Main extends Component {
     }
   }
 
-    onDishSelect(dishId) {
-        this.setState({selectedDishId: dishId});
-    }
+    // onDishSelect(dishId) {
+    //     this.setState({selectedDishId: dishId});
+    // }
 
   render() {
+    const HomePage = () => {
+        return(
+            <Home />
+        )
+    }
     return (
     <div>
         <Header/>
-        <Menu dishes={this.state.dishes}
+            <Routes>
+                <Route path="/home" element={<Home/>} />
+                <Route exact path="/menu" element={<Menu dishes={this.state.dishes}/>}/>
+                <Route path="/" element={<Navigate to="/home"/>}/>
+            </Routes>
+        {/* <Menu dishes={this.state.dishes}
             onClick={(dish) => this.onDishSelect(dish)}
-        />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDishId)[0]}/>
+        /> */}
+        {/* <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDishId)[0]}/> */}
         <Footer/>
     </div>
     );
